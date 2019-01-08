183ded6aaaa7d96508d55bde66a28b29c15c3176
sushant-waghalkar <45731823+sushant-waghalkar@users.noreply.github.com>
2019-01-08 21:46:39 +0530

    Update home.component.ts
    Added comments

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -16,16 +16,34 @@ export class HomeComponent implements OnInit {
         this.currentUser = JSON.parse(localStorage.getItem('currentUser'));
     }
 
+    //On page load call loadAllUsers method to show user list in DOM 
     ngOnInit() {
         this.showNav = true;
         this.loadAllUsers();
     }
 
+    
+    /**
+    * This is deleteUser method delete current user
+    
+    * @param This is id parameter contain current user id type of number
+
+    * @return This method does not return any value
+
+    */
     deleteUser(id: number) {
         this.userService.delete(id).subscribe(() => { this.loadAllUsers() });
     }
     
     //call the service getall from userService for display all user list
+    /**
+    * This is loadAllUsers method
+    
+    * @param This method does not contain parameter
+
+    * @return This method does not return any value
+
+    */
     private loadAllUsers() {
         this.userService.getAll().subscribe(users => { this.users = users; });
     }
