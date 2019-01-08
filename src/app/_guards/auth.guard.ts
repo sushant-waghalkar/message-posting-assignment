61bf9c00cbe6e74da34dfee403f33893340cb155
sushant-waghalkar <45731823+sushant-waghalkar@users.noreply.github.com>
2019-01-08 21:53:54 +0530

    Update auth.guard.ts
    Added Comment

--- src/app/_guards/auth.guard.ts
+++ src/app/_guards/auth.guard.ts
@@ -1,4 +1,4 @@
-﻿import { Injectable } from '@angular/core';
+import { Injectable } from '@angular/core';
 import { Router, CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot } from '@angular/router';
 
 @Injectable()
@@ -6,6 +6,14 @@ export class AuthGuard implements CanActivate {
 
     constructor(private router: Router) { }
 
+    /**
+    * This is canActivate method for checking user login or not
+    
+    * @param Contain route and state with type of ActivatedRouteSnapshot and RouterStateSnapshot
+
+    * @return return true as type boolean
+
+    */
     canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot) {
         if (localStorage.getItem('currentUser')) {
             // logged in so return true
