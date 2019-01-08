4e451893e970fa254c5ac14c80e9e7d83e22f88f
sushant-waghalkar <45731823+sushant-waghalkar@users.noreply.github.com>
2019-01-08 21:50:44 +0530

    Update authentication.service.ts
    Added comments

--- src/app/_services/authentication.service.ts
+++ src/app/_services/authentication.service.ts
@@ -1,4 +1,4 @@
-﻿import { Injectable } from '@angular/core';
+import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Observable } from 'rxjs/Observable';
 import 'rxjs/add/operator/map'
@@ -9,10 +9,27 @@ export class AuthenticationService {
     constructor(private http: HttpClient) { }
     private loggedIn: BehaviorSubject<boolean> = new BehaviorSubject<boolean>(false);
 
+    /**
+    * This is isLoggedIn method for checking user login status
+    
+    * @param This method do not contain parameter
+
+    * @return return loggedIn of type observable
+
+    */
     get isLoggedIn() {
     return this.loggedIn.asObservable();
     }
 
+
+    /**
+    * This is login method for checking user credintial
+    
+    * @param contain arguments username and password type of string
+
+    * @return This method do not return any value
+
+    */
     login(username: string, password: string) {
         return this.http.post<any>('/api/authenticate', { username: username, password: password })
             .map(user => {
@@ -27,8 +44,17 @@ export class AuthenticationService {
             });
     }
 
+    
+    /**
+    * This is logout method for remove user from local storage to log out
+    
+    * @param This method do not contain parameter
+
+    * @return This method do not return any value
+
+    */
     logout() {
-        // remove user from local storage to log user out
+        // 
         this.loggedIn.next(false);
         localStorage.removeItem('currentUser');
     }
