3250853d1f64590942185cb27b10f08624471323
sushant-waghalkar <45731823+sushant-waghalkar@users.noreply.github.com>
2019-01-08 09:12:04 +0530

    Update socket.service.ts
    Added comments to above the function

--- src/app/messanger/shared/services/socket.service.ts
+++ src/app/messanger/shared/services/socket.service.ts
@@ -12,20 +12,24 @@ const SERVER_URL = 'http://localhost:8080';
 export class SocketService {
     private socket;
 
+    //initSocket() create instance of socketIo
     public initSocket(): void {
         this.socket = socketIo(SERVER_URL);
     }
 
+    //Get msg content from chat.component and then emit msg content 
     public send(message: Message): void {
         this.socket.emit('message', message);
     }
 
+    //subscribe message data from observer    
     public onMessage(): Observable<Message> {
         return new Observable<Message>(observer => {
             this.socket.on('message', (data: Message) => observer.next(data));
         });
     }
 
+    //show server status like connect and disconnect
     public onEvent(event: Event): Observable<any> {
         return new Observable<Event>(observer => {
             this.socket.on(event, () => observer.next());
